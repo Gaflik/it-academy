c9bb09c7288847c28cfa404304394c249c1924d4
Galina Petrova <gala.po4ta@gmail.com>
2023-01-24 14:58:23 +0300

    create first file

--- lesson-04/node.js
+++ lesson-04/node.js
@@ -1,11 +1,14 @@
 console.log(global);
 
-function sayHi() {
-    return 10+30;
-}
+//function sayHi() {
+//    return 10+30;
+//}
 
-console.log(sayHi())
+//console.log(sayHi())
 
 const fs = require('fs-extra');
 
-fs.mkdirpSync('first-dir')
+//fs.mkdirpSync('first-dir')
+
+const file = 'first-dir/1file.txt'
+fs.ensureFileSync(file)
