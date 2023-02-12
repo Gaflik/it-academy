fa8838b00a238b4b439a8ddb364b4229fccb69dc
Galina Petrova <gala.po4ta@gmail.com>
2023-02-12 19:21:48 +0300

    new task lesson-07 Edited

--- lesson-07/task.js
+++ lesson-07/task.js
@@ -1,20 +1,20 @@
 // Эмулировать игру в кубики, 2 человека по очереди бросают кубик, каждый в итоге по 3 раза. У кого сумма трех бросков будет наибольшей - тот выиграл. 
 // Если суммы равны то ничья
 
-function playerThrow() {
-    return Math.ceil(Math.random() * 6);
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
 }
 
 let summ1 = 0;
 let summ2 = 0;
 
 for (let i = 0; i < 3; i++) {
-    player1 = playerThrow();
+    player1 = rollDice();
     summ1 += player1;
 }
 
 for (let k = 0; k < 3; k++) {
-        player2 = playerThrow();
+        player2 = rollDice();
         summ2 += player2;
 } 
 
@@ -30,10 +30,10 @@ if (summ1 > summ2) {
 // 2. Подсчитать количество Пятниц 13-ого с 01/01/2000 до сегодня. (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date )
 
 let startDate = new Date('2000-01-01T00:00:00.000Z')
-let endDate = new Date('2023-01-20T00:00:00.000Z')
+let endDate = new Date()
 let numOfDays = 0;
 
-function fridaysOf13() {
+function countFridayThe13th() {
     for (let i = startDate; i <= endDate; startDate.setDate(startDate.getDate() + 1)) {
         if (startDate.getDate() === 13 && startDate.getDay() === 5) {
             numOfDays++;
@@ -41,7 +41,6 @@ function fridaysOf13() {
     }
     return numOfDays
 }
-console.log(fridaysOf13())
 
 
 // 3. Напишите код который будет разбивать число на заданное количество рандомных чисел сумма которых будет равна 
@@ -62,11 +61,10 @@ for (let i = 1; i <= parts; i++) {
     numArr.push(partOfNum);
     num -= partOfNum;
 }
-console.log(numArr)
-function sumOfnumParts() {
+
+function countSumOfnumParts() {
     return numArr.reduce((accumulator,currentValue) => accumulator + currentValue)
 }
-console.log(sumOfnumParts())
 
 
 // б. числа разбивки дробные с 2 знаками после запятой (4.55, 5.20, 5.25)
@@ -85,9 +83,8 @@ for (let i = 1; i <= parts2; i++) {
     numArr2.push(partOfNum2);
     num2 -= partOfNum2;
 }
-console.log(numArr2)
-function sumOfnumParts2() {
+
+function countSumOfnumParts2() {
     return numArr2.reduce((accumulator,currentValue) => accumulator + currentValue)
 }
-const sumOfnumParts2round = Math.round(sumOfnumParts2());
-console.log(sumOfnumParts2round)
+const sumOfnumParts2round = Math.round(countSumOfnumParts2());
